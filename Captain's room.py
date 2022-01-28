a87c3166744328bb2e395c7478f332d1b60b8b4f
Panchal Jay <87118280+jaypanchal1706@users.noreply.github.com>
2022-01-28 21:59:59 +0530

    Update and rename Captain's_room.py to Captain's room.py

--- Captain's room.py
+++ Captain's room.py
@@ -1,4 +1,6 @@
 """
+Name = Panchal Jay Manojkumar
+Id = 20CE068
 Mr. Anant Asankhya is the manager at the INFINITE hotel. The hotel has an infinite amount of rooms.
 
 One fine day, a finite number of tourists come to stay at the hotel.
@@ -14,14 +16,24 @@ Mr. Anant has an unordered list of randomly arranged room entries. The list cons
 Mr. Anant needs you to help him find the Captain's room number.
 The total number of tourists or the total number of groups of families is not known to you.
 You only know the value of K and the room number list.
+
+Github link = https://github.com/jaypanchal1706/Python_assignment2.git
 """
 
+# Take the input from the user
+# k is the family member per group
 K = int(input())
+# Declare set in which room number can be inerted
 set_S = set()
-sumlist_S = 0
-for i in input().split():
-    I = int(i)
-    set_S.add(I)
-    sumlist_S += I
+# sumlist set is use for sum of all the room number
+sumlist = 0
+# Scan the all the room number
 
-print((sum(set_S)*K - sumlist_S)//(K-1))
+for i in input().split():
+    room_number = int(i)
+    set_S.add(room_number)
+    sumlist += room_number
+
+# Print captain's room number
+captain_room = int((sum(set_S) * K - sumlist) // (K - 1))
+print(captain_room)
